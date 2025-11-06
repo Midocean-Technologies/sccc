4ac796a4d75cacbc5f0c3b04d5f20b0f03c45707
Bhavesh Mahavar <bhavesh.m@midocean.tech>
2025-11-06 17:35:41 +0530

    role added from backend

--- sccc/install.py
+++ sccc/install.py
@@ -5,6 +5,13 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 @frappe.whitelist()
 def after_install():
     customfield()
+    create_role_profile()
+
+def create_role_profile():
+    if not frappe.db.exists("Role", "SCCC Admin Team"):
+        doc = frappe.new_doc("Role")
+        doc.role_name = "SCCC Admin Team"
+        doc.insert(ignore_permissions=True)
 
 def customfield():
     if "bench_manager" in frappe.get_installed_apps():
